6fbe9bf9c032c35a876a7588813fd9231b656019
trananh-it-hust <haianhtranmanh@gmail.com>
2023-10-13 22:41:15 +0700

    a

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./index.css";
 import "../../Data/data.json";
-<<<<<<< HEAD
 const Card = ({ data }) => {
   const { name, img_1, img2, discount, cost, old_cost, type, id, barcode } =
     data;
@@ -21,36 +20,6 @@ const Card = ({ data }) => {
         <div className="cost">
           <p>{cost} ₫</p>
           <del>{old_cost}₫</del>
-=======
-const Card = (prop) => {
-    const { name, img_1, img2, discount, cost, old_cost, type, id, barcode } =
-        prop.data;
-    return (
-        <div className="card" key={id}>
-            <span>{discount}</span>
-            <div className="icon">
-                <i class="icon_item fa-regular fa-heart"></i>
-                <i class="icon_item item_hidden fa-solid fa-magnifying-glass"></i>
-                <i class="icon_item item_hidden fa-solid fa-cart-shopping"></i>
-            </div>
-            <div className="card_img">
-                <img src={img_1} alt={name} />
-            </div>
-            <div className="info">
-                <h2>{name}</h2>
-                <div className="cost">
-                    <p>{cost} ₫</p>
-                    <del>{old_cost}₫</del>
-                </div>
-                <div className="star">
-                    <i class="fa-regular fa-star"></i>
-                    <i class="fa-regular fa-star"></i>
-                    <i class="fa-regular fa-star"></i>
-                    <i class="fa-regular fa-star"></i>
-                    <i class="fa-regular fa-star"></i>
-                </div>
-            </div>
->>>>>>> d867d49c6ce0b8fd3db58deac3b33dccd8c61278
         </div>
         <div className="star">
           <i className="fa-regular fa-star"></i>
