f0a9eaeb806b3a898f9d3c6ebe74adab2601de52
Lnismeee <86096138+Lnismeee@users.noreply.github.com>
2023-10-15 21:28:10 +0700

    ngoc3

--- src/Page/Home/Home_right/index.jsx
+++ src/Page/Home/Home_right/index.jsx
@@ -1,34 +1,46 @@
 import React, { useState } from "react";
 
 import "./styles.css";
-import { girl } from "./listBaby.json";
-import { boy } from "./listBaby.json";
-import { hot } from "./listBaby.json";
+import { girl, boy, hot, bear, sale } from "./listBaby.json";
+
 import Card from "../../../components/Card/Card";
+import Slideshow from "../../../components/Slider";
 const Home_right = () => {
+    var tetAmLich = new Date(2023, 9, 16, 0, 0, 0).getTime();
+
+    function newYear() {
+        let ngayHienTai = new Date().getTime();
+        let thoigianConLai = tetAmLich - ngayHienTai;
+        let giay = 1000;
+        let phut = giay * 60;
+        let gio = phut * 60;
+        let ngay = gio * 24;
+        let h = Math.floor((thoigianConLai % ngay) / gio);
+        let m = Math.floor((thoigianConLai % gio) / phut);
+        let s = Math.floor((thoigianConLai % phut) / giay);
+        document.getElementById("hour").innerText = h;
+        document.getElementById("minute").innerText = m;
+        document.getElementById("second").innerText = s;
+    }
+    setInterval(function () {
+        newYear();
+    }, 1000);
     return (
         <>
             <div className="right">
                 <div className="right__first">
-                    <div className="right__slider">
+                    <div className="r-f-l">
+                        <Slideshow></Slideshow>
+                    </div>
+                    <div className="r-f-r">
                         <img
                             src="https://bizweb.dktcdn.net/thumb/grande/100/488/521/themes/913255/assets/home_main_slider_banner_image_1.jpg?1695187373377"
                             alt=""
                         />
-                    </div>
-                    <div className="right__banner">
-                        {/* <div className="item2">
-                            <img
-                                src="https://bizweb.dktcdn.net/thumb/grande/100/488/521/themes/913255/assets/home_main_slider_banner_image_1.jpg?1695187373377"
-                                alt=""
-                            />
-                        </div>
-                        <div className="item3">
-                            <img
-                                src="https://bizweb.dktcdn.net/thumb/grande/100/488/521/themes/913255/assets/home_main_slider_banner_image_1.jpg?1695187373377"
-                                alt=""
-                            />
-                        </div> */}
+                        <img
+                            src="https://bizweb.dktcdn.net/thumb/grande/100/488/521/themes/913255/assets/home_main_slider_banner_image_2.jpg?1695187373377"
+                            alt=""
+                        />
                     </div>
                 </div>
                 <div className="right__second">
@@ -57,9 +69,8 @@ const Home_right = () => {
                                     Hoàn 200% sản phẩm kém chất lượng
                                 </span>
                                 <span>
-                                    Các đơn hàng trên 1.000.000đ chúng tôi sẽ
-                                    freeship và hoàn trả nếu như không đúng với
-                                    sản phẩm đặt.
+                                    Chúng tôi hoàn 200% nếu sản phẩm không đúng
+                                    với mẫu mã và kém chất lượng .
                                 </span>
                             </div>
                         </li>
@@ -73,13 +84,48 @@ const Home_right = () => {
                                     Hotline: 1900.63.60.99
                                 </span>
                                 <span>
-                                    Các đơn hàng trên 1.000.000đ chúng tôi sẽ
-                                    freeship và hoàn trả nếu như không đúng với
-                                    sản phẩm đặt.
+                                    Hãy gọi cho chúng tôi khi bạn cần, chúng tôi
+                                    luôn lắng nghe ý kiến của bạn.
                                 </span>
                             </div>
                         </li>
                     </ul>
+                </div>
+                <div className="r-bear">
+                    <ul className="bear__list">
+                        {bear.map((item) => (
+                            <li className="bear__item" key={item.id}>
+                                <a href="/">
+                                    <img
+                                        src={item.img}
+                                        alt={item.name}
+                                        className="bear__img"
+                                    />
+                                    <p className="bear__desc">{item.name}</p>
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div class="count">
+                    <div class="countdown">
+                        <img
+                            src="https://theme.hstatic.net/200000036162/1000967879/14/fsale_countdown_label.jpg?v=75"
+                            alt=""
+                            className="c-img"
+                        />
+                        <h2 className="c-h">kết thúc sau</h2>
+                        <div id="hour"></div>
+                        <div id="minute"></div>
+                        <div id="second"></div>
+                    </div>
+                </div>
+                <div className="r-img-banner">
+                    <img
+                        src="https://bizweb.dktcdn.net/100/488/521/themes/913255/assets/home_main_banner.jpg?1695187373377"
+                        alt=""
+                        className="r-img-i"
+                    />
                 </div>
                 <div className="r-baby">
                     <ul className="baby__list">
@@ -98,14 +144,12 @@ const Home_right = () => {
                     </ul>
                 </div>
                 <div className="r-trend">
-                    <h2>#Hot#Trend#Xuhuong</h2>
+                    <h2 className="r-t-heading">#Hot #Trend #Xuhuong</h2>
                     <ul className="r-t-list">
-                        <Card data={hot[1]} />
-                        <Card data={hot[1]} />
-
-                        <Card data={hot[1]} />
-
-                        <Card data={hot[1]} />
+                        <Card data={hot[0]} className="r-t-item" />
+                        <Card data={hot[1]} className="r-t-item" />
+                        <Card data={hot[2]} className="r-t-item" />
+                        <Card data={hot[3]} className="r-t-item" />
                     </ul>
                 </div>
                 <div className="r-baby">
@@ -120,6 +164,36 @@ const Home_right = () => {
                                     />
                                     <h3 className="baby__desc">{item.name}</h3>
                                 </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="r-sale">
+                    <h2 className="r-t-heading">Bán chạy nhất</h2>
+                    <ul className="sale__list">
+                        {sale.map((item) => (
+                            <li className="sale__card" key={sale.id}>
+                                <img
+                                    src={item.img_1}
+                                    alt={item.name}
+                                    className="sale__img"
+                                />
+
+                                <div className="sale__info">
+                                    <p className="sale__heading">{item.name}</p>
+                                    <div className="sale__cost">
+                                        <span>{item.cost}₫</span>
+                                        <del>{item.old_cost}₫</del>
+                                        <span>({item.discount})</span>
+                                    </div>
+                                    <div className="fifth__star">
+                                        <i className="fa-regular fa-star"></i>
+                                        <i className="fa-regular fa-star"></i>
+                                        <i className="fa-regular fa-star"></i>
+                                        <i className="fa-regular fa-star"></i>
+                                        <i className="fa-regular fa-star"></i>
+                                    </div>
+                                </div>
                             </li>
                         ))}
                     </ul>
--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,95 +1,30 @@
-import { useState, useEffect } from "react";
+import React from "react";
+import { Fade } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
 import "./styles.css";
-const slideStyles = {
-    width: "100%",
-    height: "100%",
-    borderRadius: "10px",
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-};
+const fadeImages = [
+    {
+        url: "https://bizweb.dktcdn.net/thumb/1024x1024/100/488/521/themes/913255/assets/home_main_slider_item_image_1.jpg?1695187373377",
+    },
+    {
+        url: "https://bizweb.dktcdn.net/thumb/1024x1024/100/488/521/themes/913255/assets/home_main_slider_item_image_2.jpg?1695187373377",
+    },
+    {
+        url: "https://bizweb.dktcdn.net/thumb/1024x1024/100/488/521/themes/913255/assets/home_main_slider_item_image_3.jpg?1695187373377",
+    },
+];
 
-const rightArrowStyles = {
-    position: "absolute",
-    top: "50%",
-    transform: "translate(0, -50%)",
-    right: "32px",
-    fontSize: "45px",
-    color: "#fff",
-    zIndex: 1,
-    cursor: "pointer",
-};
-
-const leftArrowStyles = {
-    position: "absolute",
-    top: "50%",
-    transform: "translate(0, -50%)",
-    left: "32px",
-    fontSize: "45px",
-    color: "#fff",
-    zIndex: 1,
-    cursor: "pointer",
-};
-
-const sliderStyles = {
-    position: "relative",
-    height: "100%",
-};
-
-const dotsContainerStyles = {
-    display: "flex",
-    justifyContent: "center",
-};
-
-const dotStyle = {
-    margin: "0 3px",
-    cursor: "pointer",
-    fontSize: "20px",
-};
-
-const Slider = ({ slides }) => {
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const goToPrevious = () => {
-        const isFirstSlide = currentIndex === 0;
-        const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-        setCurrentIndex(newIndex);
-    };
-    const goToNext = () => {
-        const isLastSlide = currentIndex === slides.length - 1;
-        const newIndex = isLastSlide ? 0 : currentIndex + 1;
-        setCurrentIndex(newIndex);
-    };
-    const goToSlide = (slideIndex) => {
-        setCurrentIndex(slideIndex);
-    };
-    const slideStylesWidthBackground = {
-        ...slideStyles,
-        backgroundImage: `url(${slides[currentIndex].url})`,
-    };
-
+const Slideshow = () => {
     return (
-        <div style={sliderStyles}>
-            <div>
-                <div onClick={goToPrevious} style={leftArrowStyles}>
-                    ❰
-                </div>
-                <div onClick={goToNext} style={rightArrowStyles}>
-                    ❱
-                </div>
-            </div>
-            <div style={slideStylesWidthBackground}></div>
-            <div style={dotsContainerStyles}>
-                {slides.map((slide, slideIndex) => (
-                    <div
-                        style={dotStyle}
-                        key={slideIndex}
-                        onClick={() => goToSlide(slideIndex)}
-                    >
-                        ●
+        <div className="slide-container">
+            <Fade>
+                {fadeImages.map((fadeImage, index) => (
+                    <div key={index}>
+                        <img src={fadeImage.url} />
                     </div>
                 ))}
-            </div>
+            </Fade>
         </div>
     );
 };
-
-export default Slider;
+export default Slideshow;
